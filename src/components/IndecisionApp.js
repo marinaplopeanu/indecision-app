bb633d560697389a77961335fe05574fedf60427
Marina Plopeanu <Marina.Plopeanu@fwwebbcorp.fwwebb.com>
2019-09-30 16:30:13 -0400

    added style

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -1,51 +1,47 @@
-import React from 'react';
-import AddOption from './AddOption';
-import Options from './Options';
-import Header from './Header';
-import Action from './Action';
-import OptionModal from './OptionModal';
+import React from "react";
+import AddOption from "./AddOption";
+import Action from "./Action";
+import Header from "./Header";
+import Options from "./Options";
+import OptionModal from "./OptionModal";
 
-
-class IndecisionApp extends React.Component {
-
+export default class IndecisionApp extends React.Component {
   state = {
     options: [],
     selectedOption: undefined
   };
-
-  handleClearSelectedOption = () => {
-    this.setState(() => ({selectedOption:undefined}))
-  }
   handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
   };
-  handleDeleteOption = (optionToRemove) => {
-    this.setState((prevState) => ({
-      options: prevState.options.filter((option) => optionToRemove !== option)
+  handleClearSelectedOption = () => {
+    this.setState(() => ({ selectedOption: undefined }));
+  };
+  handleDeleteOption = optionToRemove => {
+    this.setState(prevState => ({
+      options: prevState.options.filter(option => optionToRemove !== option)
     }));
   };
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    this.setState({
+    this.setState(() => ({
       selectedOption: option
-    })
+    }));
   };
-  handleAddOption = (option) => {
+  handleAddOption = option => {
     if (!option) {
-      return 'Enter valid value to add item';
+      return "Enter valid value to add item";
     } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists';
+      return "This option already exists";
     }
 
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       options: prevState.options.concat(option)
     }));
   };
-
   componentDidMount() {
     try {
-      const json = localStorage.getItem('options');
+      const json = localStorage.getItem("options");
       const options = JSON.parse(json);
 
       if (options) {
@@ -58,32 +54,33 @@ class IndecisionApp extends React.Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevState.options.length !== this.state.options.length) {
       const json = JSON.stringify(this.state.options);
-      localStorage.setItem('options', json);
+      localStorage.setItem("options", json);
     }
   }
   componentWillUnmount() {
-    // console.log('componentWillUnmount');
+    console.log("componentWillUnmount");
   }
- 
   render() {
-    const subtitle = 'Put your life in the hands of a computer';
+    const subtitle = "Put your life in the hands of a computer";
 
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-        <OptionModal 
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption handleAddOption={this.handleAddOption} />
+          </div>
+        </div>
+        <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
         />
@@ -91,5 +88,3 @@ class IndecisionApp extends React.Component {
     );
   }
 }
-
-export default IndecisionApp;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require('path');
 
-
-console.log(__dirname);
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname,'public'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
@@ -13,10 +11,17 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
     }]
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname,'public')
+    contentBase: path.join(__dirname, 'public')
   }
 };
